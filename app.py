7118c80a2d5a8269401c7f7df6de3ac4e39697cb
Diallo <mamadouadama609@gmail.com>
2024-07-19 22:33:45 -0500

    add path variable

--- app.py
+++ app.py
@@ -8,5 +8,19 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
+@app.route('/')
+@app.route('/home')
+def home():
+    return 'Hello, World!'
+
+
+@app.route('/reporter/<int:reporter_id>')
+def reporter(reporter_id):
+    return f'''
+    <h2>Reporter {reporter_id} Bio</h2>
+    <a href="/">Return to home page</a>
+    '''
+
+
 if __name__ == '__main__':
     app.run()
